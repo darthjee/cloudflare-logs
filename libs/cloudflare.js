805816322f07dd009973a8cba836be1399a3b8d9
darthjee <darthjee@gmail.com>
2016-10-28 17:55:53 -0200

    Extract api

--- libs/cloudflare.js
+++ libs/cloudflare.js
@@ -1,48 +1,17 @@
 (function(module){
-  var http = require('https');
   var _ = require("underscore");
+  var CloudflareApi = require('./cloudflare_api')
 
   function Cloudflare(zoneId, authEmail, authKey) {
-    this.zoneId = zoneId;
-    this.authEmail = authEmail;
-    this.authKey = authKey;
+    this.api = new CloudflareApi(zoneId, authEmail, authKey);
   }
 
   var fn = Cloudflare.prototype;
 
   fn.fetch = function() {
-    http.get({
-      hostname: 'api.cloudflare.com',
-      path: '/client/v4/zones/' + this.zoneId + '/logs/requests' + this._params(),
-      headers: this._headers()
-    }, this._readResponse);
-  };
-
-  fn._params = function() {
-    return '?count=1&start=' + this.startTime();
-  };
-
-  fn.startTime = function() {
-    return Math.floor((new Date().getTime() / 1000) - 120)
-  };
-
-  fn._readResponse = function(response) {
-    var body = '';
-    response.on('end', function() {
-      console.info(JSON.parse(body));
+    this.api.logs(function(json) {
+       console.info(json);
     });
-    response.on('data', function(chunk) {
-      body += chunk;
-    });
-  };
-
-  fn._headers = function() {
-    var headers = {};
-    //headers['Accept-Encoding'] = 'gzip';
-    headers['X-Auth-Email'] = this.authEmail;
-    headers['X-Auth-Key'] = this.authKey;
-
-    return headers;
   };
 
   module.exports = Cloudflare;
---
+++ libs/cloudflare_api.js
@@ -0,0 +1,51 @@
+(function(module){
+  var http = require('https');
+
+  function CloudflareApi(zoneId, authEmail, authKey) {
+    this.zoneId = zoneId;
+    this.authEmail = authEmail;
+    this.authKey = authKey;
+  }
+
+  var fn = CloudflareApi.prototype;
+
+  fn.logs = function(callback) {
+    http.get({
+      hostname: 'api.cloudflare.com',
+      path: '/client/v4/zones/' + this.zoneId + '/logs/requests' + this._params(),
+      headers: this._headers()
+    }, this._buildResponseParser(callback));
+  };
+
+  fn._params = function() {
+    return '?count=10000&start=' + this.startTime();
+  };
+
+  fn.startTime = function() {
+    return Math.floor((new Date().getTime() / 1000) - 120)
+  };
+
+  fn._buildResponseParser = function(callback) {
+    return function(response) {
+      var body = '';
+      response.on('end', function() {
+        callback.call(this, JSON.parse(body));
+      });
+      response.on('data', function(chunk) {
+        body += chunk;
+      });
+    };
+  };
+
+  fn._headers = function() {
+    var headers = {};
+    //headers['Accept-Encoding'] = 'gzip';
+    headers['X-Auth-Email'] = this.authEmail;
+    headers['X-Auth-Key'] = this.authKey;
+
+    return headers;
+  };
+
+  module.exports = CloudflareApi;
+})(module);
+
