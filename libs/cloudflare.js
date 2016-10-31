e88d5a5785c05a38eb00197d04fe83683494bcd4
darthjee <darthjee@gmail.com>
2016-10-31 01:07:26 -0200

    Download json in blocks

--- libs/cloudflare.js
+++ libs/cloudflare.js
@@ -1,29 +1,50 @@
 (function(module){
-  var _ = require("underscore");
-  var CloudflareApi = require('./cloudflare_api')
+  var _ = require("underscore"),
+     CloudflareApi = require('./cloudflare_api'),
+     Repeater = require('./repeater');
 
   function Cloudflare(zoneId, authEmail, authKey) {
     this.api = new CloudflareApi(zoneId, authEmail, authKey);
+    this.loadSize = 100;
+
+    _.bindAll(this, '_repeaterCallback');
+    this.repeater = new Repeater(this._repeaterCallback, this, 5);
   }
 
   var fn = Cloudflare.prototype;
 
-  fn.fetch = function() {
+  fn.fetchAll = function() {
+    this.repeater.call();
+  };
+
+  fn.fetch = function(callback) {
     this.api.logs({
-      count: 10000,
+      count: this.loadSize,
       start: this.startTime()
     }, {
-      process: function(json) {
-        console.info(json);
-      },
-      finish: function(count) {
-        console.info(count);
-      }
+      process: this._process,
+      finish: this._finish
     });
   };
 
+  fn._repeaterCallback = function(callback) {
+    var that = this;
+
+    this.fetch(function(count){
+      callback(count >= that.loadSize / 2)
+    });
+  };
+
+  fn._process = function(json) {
+  };
+  
+  
+  fn.finish = function(count) {
+    callback.call(this, count);
+  };
+
   fn.startTime = function() {
-    return Math.floor((new Date().getTime() / 1000) - 120)
+    return Math.floor((new Date().getTime() / 1000) - 3600)
   };
 
   module.exports = Cloudflare;
