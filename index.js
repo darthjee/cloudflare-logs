0999283e7ceb2f06ef56266159cac8c04a5b36ca
darthjee <darthjee@gmail.com>
2016-11-01 02:31:36 -0200

    Move handler to index

---
+++ index.js
@@ -0,0 +1,21 @@
+(function(module) {
+  var Cloudflare = require('./libs/cloudflare'),
+      BigQueryApi = require('./libs/big_query/api');
+
+  module.exports = {
+    handler: function() {
+      const awsConfig = require('./aws.json');
+      const projectData = require('./auth.json');
+      const bigQueryConfig = {
+        projectId: projectData['project_id'],
+        keyFilename: './auth.json'
+      };
+
+      BigQueryApi.default = new BigQueryApi(bigQueryConfig).connect();
+
+      cloudflare = new Cloudflare(awsConfig);
+
+      cloudflare.fetchAll();
+    }
+  };
+});
